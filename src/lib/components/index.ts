f4dd953c8a820e3c807a41ca55555083c44120f0
Azuriru <kegumine@gmail.com>
2024-02-04 19:22:30 +0800

    update

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,4 +1,1 @@
-export { default as BackButton } from './BackButton.svelte';
 export { default as FontAwesome } from './FontAwesome.svelte';
-export { default as Screen } from './Screen.svelte';
-export { default as TransitionScreen } from './TransitionScreen.svelte';
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,49 +1,11 @@
 <script lang="ts">
     import './styles.css';
-    // import { beforeNavigate, onNavigate } from '$app/navigation';
-    import { transitioning, onKeydown } from '$lib/util/store';
-    import { /* Screen, */ TransitionScreen } from '$lib/components';
-
-// let transitioned: Promise<void>;
-
-	// beforeNavigate(async nav => {
-	// 	const { to, from } = nav;
-
-	// 	if (to?.route.id === from?.route.id) {
-	// 		nav.cancel();
-	// 		return;
-	// 	};
-
-	// 	transitioned = new Promise((res) => transitioning.set(res));
-	// });
-
-	// onNavigate(async () => {
-	// 	await transitioned;
-	// 	transitioning.set(null);
-	// });
 </script>
 
 <svelte:head>
     <title>List</title>
 </svelte:head>
 
-<svelte:window on:keydown={$onKeydown} />
-
 <div class="main-screen">
     <slot />
 </div>
-
-{#if $transitioning}
-    <TransitionScreen />
-{/if}
-
-<style lang="scss">
-	.main-screen {
-        position: absolute;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        background-color: black;
-	}
-</style>
