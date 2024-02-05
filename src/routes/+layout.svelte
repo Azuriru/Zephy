3b2b4cf0a3d364c73ec2bd9ec9ad2e87775269ae
Azuriru <jeannelee823@gmail.com>
2024-02-05 10:46:24 +0800

    try service worker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
     import './styles.css';
+
+    if ('serviceWorker' in navigator) {
+        console.log('service worker');
+        addEventListener('load', function () {
+            navigator.serviceWorker.register('./service-worker.js');
+        });
+    }
 </script>
 
 <svelte:head>
