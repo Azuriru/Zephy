f14ac2d78def9b0b9bee6b056a7c677ce5a9d026
Azuriru <kegumine@gmail.com>
2024-02-10 23:09:53 +0800

    List looks fucking fabulous

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,39 +18,13 @@
     type List = Group[];
     let list = localStorageCentralized<List>('list', sample);
 
-    // function onpaste(e: ClipboardEvent) {
-    //     e.preventDefault();
+    // type HistoryEntry = {
 
-    //     const selection = window.getSelection();
-    //     // Don't allow deleting nodes
-    //     if (selection && selection?.anchorNode.isSameNode(selection.focusNode)) {
-    //         // get text representation of clipboard
-    //         const text = e.clipboardData.getData('text/plain');
-
-    //         // insert text manually, but without new line characters as can't support <br/>s yet
-    //         document.execCommand('insertHTML', false, text.replace(/\n/g, ''));
-    //     }
     // }
 
-    function _onKeyDown(e: KeyboardEvent) {
-        const selection = window.getSelection();
+    let historyList = [];
 
-        switch (e.key) {
-            case 'Enter':
-                e.preventDefault();
-                break;
-            case 'Backspace':
-            case 'Delete':
-            case 'Paste':
-                // Don't allow deleting nodes
-                if (!selection?.anchorNode?.isSameNode(selection.focusNode)) {
-                    e.preventDefault();
-                }
-                break;
-        }
-    }
-
-    function formatTimestamp(timestamp: number | undefined) {
+    function formatTimestamp(timestamp?: number) {
         if (!timestamp) {
             return '';
         }
@@ -66,12 +40,24 @@
 
         const pad = (number: number) => number.toString().padStart(2, '0');
 
-        return `${pad(h)}:${pad(m)}:${pad(s)} ${pad(date)}/${pad(
-            month + 1,
-        )}/${year}`;
+        return `${pad(h)}:${pad(m)}:${pad(s)} ${pad(date)}/${pad(month + 1)}/${year}`;
     }
 
-    function onChange(groupIndex: number, itemIndex: number) {
+    function addItem(groupIndex: number) {
+        $list[groupIndex].items.push({ value: '' });
+        $list = $list;
+    }
+
+    function removeItem(groupIndex: number, itemIndex: number) {
+        $list[groupIndex].items.splice(itemIndex, 1);
+        $list = $list;
+    }
+
+    function renameItem(groupIndex: number, itemIndex: number) {
+        $list[groupIndex].items[itemIndex].value = capitalize($list[groupIndex].items[itemIndex].value);
+    }
+
+    function onCheckItem(groupIndex: number, itemIndex: number) {
         const item = $list[groupIndex].items[itemIndex];
 
         if (item.checked) {
@@ -81,21 +67,21 @@
         $list = $list;
     }
 
-    function addItem(value: string, groupIndex: number) {
-        $list[groupIndex].items.push({ value });
+    function addGroup() {
+        $list.push({
+            name: '',
+            items: []
+        });
         $list = $list;
     }
-
-    function removeItem(groupIndex: number, itemIndex: number) {
-        $list[groupIndex].items.splice(itemIndex, 1);
-        $list = $list;
-    }
-
-    function addGroup(groupName: string) {}
 
     function removeGroup(groupIndex: number) {
         $list.splice(groupIndex, 1);
         $list = $list;
+    }
+
+    function renameGroup(groupIndex: number) {
+        $list[groupIndex].name = capitalize($list[groupIndex].name);
     }
 
     function checkGroup(groupIndex: number) {
@@ -106,17 +92,17 @@
         $list = $list;
     }
 
-    function checkAll() {
-        for (const [groupIndex] of $list.entries()) {
-            checkGroup(groupIndex);
-        }
-    }
-
     function uncheckGroup(groupIndex: number) {
         for (const item of $list[groupIndex].items) {
             item.checked = false;
         }
         $list = $list;
+    }
+
+    function checkAll() {
+        for (const [groupIndex] of $list.entries()) {
+            checkGroup(groupIndex);
+        }
     }
 
     function uncheckAll() {
@@ -125,205 +111,253 @@
         }
     }
 
-    function findDuplicate(groupIndex: number, value: string) {
-        return $list[groupIndex].items.some(
-            (item: Item) => item.value === value,
-        );
-    }
-
-    function focusOut(e: FocusEvent, groupIndex: number) {
-        const { currentTarget } = e;
-        if (!(currentTarget instanceof HTMLElement)) return;
-
-        const items = currentTarget.innerText.split('\n');
-        currentTarget.innerText = '';
-
-        for (const item of items) {
-            const value = item.toLowerCase();
-            if (value && !findDuplicate(groupIndex, value)) {
-                addItem(value, groupIndex);
-            }
-            $list = $list;
-        }
-    }
-
     function clear() {
-        localStorage.removeItem('persistibles');
+        $list = sample;
     }
 </script>
 
 <div class="list">
-    <div class="list-options">
-        <button>
-            Add Group
-            <FontAwesome name="plus" />
-        </button>
-        <button>
-            Remove Group
-            <FontAwesome name="minus" />
-        </button>
-        <button on:click={uncheckAll}>
-            Uncheck all
-            <FontAwesome name="xmark" />
-        </button>
-        <button on:click={checkAll}>
-            Check all
-            <FontAwesome name="check" />
-        </button>
-    </div>
-    {#if $list}
-        {#each $list as group, groupIndex (group)}
-            {@const { name, items } = group}
-            <div class="list-group">
-                <div class="list-header">
-                    <span>Group: {capitalize(name)}</span>
-                    <div class="list-group-actions">
-                        <button on:click={() => uncheckGroup(groupIndex)}>
-                            <FontAwesome name="square" type="regular" />
-                        </button>
-                        <button on:click={() => checkGroup(groupIndex)}>
-                            <FontAwesome name="square-check" type="regular" />
-                        </button>
-                    </div>
+    {#each $list as group, groupIndex (group)}
+        {@const { items } = group}
+        <div class="list-group">
+            <div class="list-header">
+                <input type="text" placeholder="New group" bind:value={group.name} on:input={() => renameGroup(groupIndex)} />
+                <div class="list-group-actions">
+                    <button type="button" on:click={() => removeGroup(groupIndex)}>
+                        <FontAwesome name="trash-can" type="regular" />
+                    </button>
+                    <button type="button" on:click={() => uncheckGroup(groupIndex)}>
+                        <FontAwesome name="square" type="regular" />
+                    </button>
+                    <button type="button" on:click={() => checkGroup(groupIndex)}>
+                        <FontAwesome name="square-check" type="regular" />
+                    </button>
                 </div>
-                {#each items as { checked, value, timestamp }, itemIndex (value)}
-                    <label class="list-item" class:checked>
-                        <input
-                            type="checkbox"
-                            bind:checked
-                            on:change={() => onChange(groupIndex, itemIndex)}
-                        />
+            </div>
+            {#each items as item, itemIndex (item)}
+                {@const { checked, timestamp } = item}
+                <div class="list-item" class:checked>
+                    <div class="list-drag">
+                        <FontAwesome name="grip-vertical" />
+                    </div>
+                    <label class="list-label">
+                        <input type="checkbox" bind:checked={item.checked} on:change={() => onCheckItem(groupIndex, itemIndex)} />
                         <div class="list-checkbox">
                             {#if checked}
                                 <FontAwesome name="check" />
                             {/if}
                         </div>
                         <div class="list-item-info">
-                            <span class="list-item-name">{value}</span>
-                            <span class="list-item-timestamp"
-                                >{formatTimestamp(timestamp)}</span
-                            >
+                            <input
+                                class="list-item-name"
+                                type="text"
+                                placeholder="New item"
+                                bind:value={item.value}
+                                on:input={() => renameItem(groupIndex, itemIndex)}
+                            />
+                            {#if timestamp}
+                                <span class="list-item-timestamp">{formatTimestamp(timestamp)}</span>
+                            {/if}
                         </div>
-                        <button
-                            class="list-item-remove"
-                            on:click={() => removeItem(groupIndex, itemIndex)}
-                        >
-                            <FontAwesome name="xmark" />
-                        </button>
                     </label>
-                {/each}
-                <div
-                    class="list-last"
-                    contenteditable="true"
-                    on:focusout={(e) => focusOut(e, groupIndex)}
-                />
-            </div>
-        {/each}
-    {/if}
-    <button on:click={clear}>Reset</button>
+                    <button type="button" class="list-item-remove" on:click={() => removeItem(groupIndex, itemIndex)}>
+                        <FontAwesome name="xmark" />
+                    </button>
+                </div>
+            {/each}
+            <button type="button" class="list-last" on:click={() => addItem(groupIndex)}>
+                <div class="list-plus">
+                    <FontAwesome name="plus" />
+                </div>
+                Add item
+            </button>
+        </div>
+    {/each}
+    <button type="button" on:click={clear}>Reset</button>
+</div>
+
+<div class="toolbar">
+    <div class="toolbar-left">
+        <button type="button" on:click={addGroup}>
+            <FontAwesome name="square-plus" type="regular" />
+        </button>
+        <button type="button" class="history-control">
+            <FontAwesome name="arrow-rotate-left" />
+        </button>
+        <button type="button" class="history-control">
+            <FontAwesome name="arrow-rotate-right" />
+        </button>
+    </div>
+    <div class="toolbar-right">
+        <button type="button" on:click={uncheckAll}>
+            <FontAwesome name="square" type="regular" />
+        </button>
+        <button type="button" on:click={checkAll}>
+            <FontAwesome name="square-check" type="regular" />
+        </button>
+    </div>
 </div>
 
 <style lang="scss">
-    @mixin flex {
-        display: flex;
+    @use "sass:list";
+    @mixin flex($props...) {
+        @if list.index($props, hidden) {
+            display: none;
+        } @else {
+            display: flex;
+        }
+
+        @each $prop in $props {
+            @if $prop == center {
+                align-items: center;
+                justify-content: center;
+            } @else if $prop == startY {
+                align-items: flex-start;
+            } @else if $prop == centerY {
+                align-items: center;
+            } @else if $prop == endY {
+                align-items: flex-end;
+            } @else if $prop == startX {
+                justify-content: flex-start;
+            } @else if $prop == centerX {
+                justify-content: center;
+            } @else if $prop == endX {
+                justify-content: flex-end;
+            } @else if $prop == between {
+                justify-content: space-between;
+            } @else if $prop == around {
+                justify-content: space-around;
+            } @else if $prop == evenly {
+                justify-content: space-evenly;
+            } @else if $prop == column {
+                flex-direction: column;
+            } @else if $prop == 1 or $prop == one {
+                flex: 1 1 0%;
+            } @else if $prop == auto {
+                flex: 1 1 auto;
+            } @else if $prop == initial {
+                flex: 0 1 auto;
+            } @else if $prop == none {
+                flex: none;
+            } @else if $prop == noShrink {
+                flex-shrink: none;
+            }
+        }
     }
 
-    @mixin flex-col {
-        flex-direction: column;
-    }
+    $cube: 24px;
+    .toolbar {
+        @include flex(between);
+        background: #20232c;
+        padding: 8px;
+        position: sticky;
+        bottom: 0;
+        top: 100%;
+        font-size: 24px;
 
-    @mixin center-y {
-        @include flex;
-        align-items: center;
-    }
+        .toolbar-left,
+        .toolbar-right {
+            display: flex;
 
-    @mixin center-x {
-        @include flex;
-        justify-content: center;
-    }
+            button:not(:first-of-type) {
+                margin-left: 8px;
+            }
+        }
 
-    @mixin center {
-        @include flex;
-        align-items: center;
-        justify-content: center;
-    }
+        .history-control {
+            font-size: 20px;
 
-    @mixin divide-evenly {
-        display: flex;
-        justify-content: space-evenly;
+            &.disabled {
+                filter: brightness(0.5);
+            }
+        }
     }
 
     .list {
         padding: 40px;
+        overflow: auto;
+        flex-grow: 1;
     }
 
     .list,
     .list-group,
     .list-item-info {
-        display: flex;
-        flex-direction: column;
+        @include flex(column);
     }
 
-    .list-options {
-        @include divide-evenly;
-        margin-bottom: 20px;
-    }
     .list-group {
         margin-bottom: 20px;
     }
 
     .list-header {
-        display: flex;
-        justify-content: space-between;
+        @include flex(between);
         margin-bottom: 10px;
     }
 
     .list-item {
-        display: flex;
-        align-items: center;
-        padding: 12px 0;
+        @include flex(centerY);
+        height: 36px;
+
+        // &:not(:nth-last-of-type(2)) {
+            margin-bottom: 4px;
+        // }
 
         &.checked {
             order: 1;
             filter: brightness(0.5);
         }
 
-        input {
-            appearance: none;
+        .list-drag {
+            @include flex(center);
+            width: $cube;
+            height: $cube;
+            margin-right: 8px;
+        }
 
-            & + .list-checkbox {
-                @include center;
-                width: 20px;
-                height: 20px;
-                background: rgb(255, 255, 255, 0.1);
-                color: #34d399;
-                margin-right: 8px;
+        .list-label {
+            @include flex(centerY, one);
+            input {
+                appearance: none;
+
+                & + .list-checkbox {
+                    @include flex(center);
+                    width: $cube;
+                    height: $cube;
+                    background: rgb(255, 255, 255, 0.1);
+                    color: #34d399;
+                    margin-right: 8px;
+                }
+            }
+
+            .list-item-info {
+                flex-grow: 1;
+            }
+
+            .list-item-timestamp {
+                font-size: 12px;
+                color: #848484;
             }
         }
 
-        .list-item-info {
-            flex-grow: 1;
-        }
-
-        .list-item-name {
-            text-transform: capitalize;
-        }
-
-        .list-item-timestamp {
-            font-size: 12px;
-            color: #848484;
-        }
-
         .list-item-remove {
-            @include center;
+            @include flex(center);
             width: 20px;
             height: 20px;
         }
     }
 
     .list-last {
+        @include flex(centerY);
+        height: 36px;
         // background: blue;
-        margin-left: 28px;
+        margin-left: 32px;
         order: 10;
+
+        .list-plus {
+            @include flex(center);
+            width: $cube;
+            height: $cube;
+            margin-right: 8px;
+        }
     }
 </style>
