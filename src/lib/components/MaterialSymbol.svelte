ba60734bf535e48efe9e8c4ddd38077473b4eac7
Azuriru <jeannelee823@gmail.com>
2024-02-18 19:31:23 +0800

    friendship ended with fontawesome, now material symbol is my friend

---
+++ src/lib/components/MaterialSymbol.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import 'material-symbols';
+    import { type MaterialSymbol } from 'material-symbols';
+
+    export let type: 'outlined' | 'rounded' | 'sharp' = 'outlined';
+    export let name: MaterialSymbol;
+    export let size: number | null = null;
+
+    const fontWeight = type === 'outlined' ? 300 : 'initial';
+</script>
+
+<span class="material-symbol material-symbols-{type}" style:font-size={size + 'px'} style:font-weight={fontWeight}>
+    {name}
+</span>
+
+<style lang="scss">
+    .material-symbol {
+        font-size: inherit;
+    }
+</style>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,1 +1,2 @@
 export { default as FontAwesome } from './FontAwesome.svelte';
+export { default as MaterialSymbol } from './MaterialSymbol.svelte';
